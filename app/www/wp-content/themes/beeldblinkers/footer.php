b5567cabd3312a54cfcb85fd75fb0b24043c2ec5
Arjan Snaterse <arjan@uprise.nl>
2018-11-20 14:18:42 +0100

    Footer logo's

--- app/www/wp-content/themes/beeldblinkers/footer.php
+++ app/www/wp-content/themes/beeldblinkers/footer.php
@@ -1,5 +1,44 @@
-<footer class="block footer footer_block text_center">
+<?php
 
+if( ! is_front_page() ) {
+	$logos_titel = get_field( 'logos_titel', 'option' );
+	$logos       = get_field( 'logos', 'option' );
+
+	if ( ! empty( $logos ) ) {
+		?>
+        <article class="block logo_block">
+            <div class="inner">
+                <div class="content">
+                    <div class="intro">
+                        <h2><?php echo $logos_titel; ?></h2>
+                    </div>
+					<?php
+					while ( have_rows( 'logos', 'option' ) ) {
+						the_row();
+
+						$foto = get_sub_field( 'logo_afbeelding' );
+						$link = get_sub_field( 'link' );
+
+						if ( ! empty( $link ) ) {
+							if ( ! empty( $foto ) ) {
+								echo '<div class="logo-item"><a href="' . $link . '"><img src="' . $foto['url'] . '"></a></div>';
+							}
+						} else {
+							if ( ! empty( $foto ) ) {
+								echo '<div class="logo-item"><img src="' . $foto['url'] . '"></div>';
+							}
+						}
+					}
+					?>
+                </div>
+            </div>
+        </article>
+		<?php
+	}
+}
+?>
+
+<footer class="block footer footer_block text_center">
     <div class="inner">
         <div class="content">
             <div class="column">
